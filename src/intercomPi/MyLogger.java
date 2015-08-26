e1cdb4615bbca9d7b3096375e0ee8915d127a5ef
Jérémy GAUTIER <gautierjeremy@gmail.com>
2015-08-26 13:30:17 +0200

    Ajout des informations de classe et méthode.
    Ajout des informations de la classe appellante  et la méthode appellante.

--- src/intercomPi/MyLogger.java
+++ src/intercomPi/MyLogger.java
@@ -7,9 +7,23 @@ import java.util.Date;
 public class MyLogger {
 
 	private static DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd|HH:mm:ss|");
-	private static Date date = new Date();
+	//private static Date date = new Date();
 	
 	public static void log(String s){
-		System.out.println(dateFormat.format(date) + s);
+		System.out.println(dateFormat.format(new Date()) + getCallingMethod() + "|" + s);
 	}
+	
+	private static String getCallingMethod() {
+        	return trace(Thread.currentThread().getStackTrace(), 3);
+    	}
+ 
+    	private static String trace(StackTraceElement[] e, int level) {
+        	if (e != null && e.length >= level) {
+            		StackTraceElement s = e[level];
+            		if (s != null) {
+                		return s.getClassName() + "." + s.getMethodName();
+            		}
+        	}
+        return null;
+    }
 }
